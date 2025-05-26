e84d65a22fbbff18354b2a338eb4f2cbfa82d7d0
Gurmesa <gurmesakedir96@gmail.com>
2025-05-26 23:23:01 +0300

    mouse and peanut detail page

---
+++ product-detail/mouse.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product Description Page</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="../style/description.css">
+</head>
+<body>
+
+<?php
+include '../structure/header.html';
+?>
+
+    <div class="container">
+        <div class="image-gallery">
+            <img id="main-image" src="../photos/f137fa5b48b2316a17dfc34ed6062df0.jpg" alt="Main Product Image">
+            <div class="product-description">
+                <span>1 year warranty </span> <br> 
+                <span>Feel free to return!</span>
+            </div>
+        </div>
+        <div class="product-details">
+            <h1>Wireless Mouse</h1>
+            <div class="rating">
+                &#9733; &#9733; &#9733; &#9733; &#9734; (150 Reviews)
+            </div>
+            <div class="price">600</div>
+            <p>Ergonomic and precise, our wireless mouse offers seamless navigation and responsive performance. Its sleek design fits comfortably in your hand, making it perfect for work or play. Compatible with multiple devices, it’s the ideal tool for productivity on the go.</p>
+            <div class="quantity">
+                <button id="decrease">-</button>
+                <input type="text" id="quantity" value="1">
+                <button id="increase">+</button>
+            </div>
+            <div class="buttons">
+                <button class="buy-now">Buy Now</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="overlay" id="form-overlay">
+        <div class="form-container">
+            <h2>Order Details</h2>
+            <form id="order-form" action="../php/pl_order.php"  method="post">
+                <input type="email" name="email" id="email" placeholder="Your Email" required>
+                <input type="password" name="password" id="password" placeholder="Your Password" required>
+                <button type="submit" name="submit">Start Processing</button>
+            </form>
+            
+        </div>
+    </div>
+    <!-- Javascript -->
+     <script  src="../function/Descriptioin.js"></script>
+</body>
+</html>
